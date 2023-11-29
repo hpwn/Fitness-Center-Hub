8132060a89d22ffc7d0bd73bf6eccab921340add
Hayden Price <hwplivnlow@gmail.com>
2023-11-29 14:28:27 -0700

    refactor EZ insert, fix initial data entry file

--- FitnessCenterApp.java
+++ FitnessCenterApp.java
@@ -87,248 +87,89 @@ public class FitnessCenterApp {
 
     private static void insertMemberRecord() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter member ID: ");
-        int memberId = scanner.nextInt();
-        scanner.nextLine(); // consume newline
-        System.out.println("Enter first name: ");
-        String fname = scanner.nextLine();
-        System.out.println("Enter last name: ");
-        String lname = scanner.nextLine();
-        System.out.println("Enter phone number: ");
-        String phonenum = scanner.nextLine();
-        System.out.println("Enter membership level ID: ");
-        int memlevelId = scanner.nextInt();
-        System.out.println("Enter total spent: ");
-        double totalSpent = scanner.nextDouble();
+        System.out.println(
+                "Enter member ID, first name, last name, phone number, membership level ID, total spent (comma-separated): ");
+        String input = scanner.nextLine();
+        String[] params = input.split(",");
 
-        try (Connection conn = DBConnection.getConnection();
-                PreparedStatement stmt = conn
-                        .prepareStatement("INSERT INTO colegperry.member VALUES (?, ?, ?, ?, ?, ?)")) {
-            stmt.setInt(1, memberId);
-            stmt.setString(2, fname);
-            stmt.setString(3, lname);
-            stmt.setString(4, phonenum);
-            stmt.setInt(5, memlevelId);
-            stmt.setDouble(6, totalSpent);
-
-            int rowsAffected = stmt.executeUpdate();
-            System.out.println(rowsAffected + " row(s) inserted.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executeInsert("INSERT INTO colegperry.member VALUES (?, ?, ?, ?, ?, ?)", params);
     }
 
     private static void insertMemLevelRecord() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Level ID, Level Name, Discount, Minimum Spent (comma-separated): ");
+        String input = scanner.nextLine();
+        String[] params = input.split(",");
 
-        System.out.println("Enter Level ID: ");
-        int levelId = scanner.nextInt();
-        scanner.nextLine(); // consume newline
-
-        System.out.println("Enter Level Name: ");
-        String levelName = scanner.nextLine();
-
-        System.out.println("Enter Discount: ");
-        int discount = scanner.nextInt();
-
-        System.out.println("Enter Minimum Spent: ");
-        double minSpent = scanner.nextDouble();
-
-        try (Connection conn = DBConnection.getConnection();
-                PreparedStatement stmt = conn.prepareStatement("INSERT INTO colegperry.memlevel VALUES (?, ?, ?, ?)")) {
-
-            stmt.setInt(1, levelId);
-            stmt.setString(2, levelName);
-            stmt.setInt(3, discount);
-            stmt.setDouble(4, minSpent);
-
-            int rowsAffected = stmt.executeUpdate();
-            System.out.println(rowsAffected + " row(s) inserted.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executeInsert("INSERT INTO colegperry.memlevel VALUES (?, ?, ?, ?)", params);
     }
 
     private static void insertCourseRecord() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                "Enter Course ID, Course Name, Start Time, Start Date (yyyy-mm-dd), End Date (yyyy-mm-dd) (comma-separated): ");
+        String input = scanner.nextLine();
+        String[] params = input.split(",");
 
-        System.out.println("Enter Course ID: ");
-        int courseId = scanner.nextInt();
-        scanner.nextLine(); // consume newline
-
-        System.out.println("Enter Course Name: ");
-        String courseName = scanner.nextLine();
-
-        System.out.println("Enter Start Time (as an integer): ");
-        int time = scanner.nextInt();
-
-        System.out.println("Enter Start Date (format yyyy-mm-dd): ");
-        String startDate = scanner.next();
-
-        System.out.println("Enter End Date (format yyyy-mm-dd): ");
-        String endDate = scanner.next();
-
-        try (Connection conn = DBConnection.getConnection();
-                PreparedStatement stmt = conn.prepareStatement(
-                        "INSERT INTO colegperry.course VALUES (?, ?, ?, TO_DATE(?, 'YYYY-MM-DD'), TO_DATE(?, 'YYYY-MM-DD'))")) {
-
-            stmt.setInt(1, courseId);
-            stmt.setString(2, courseName);
-            stmt.setInt(3, time);
-            stmt.setString(4, startDate);
-            stmt.setString(5, endDate);
-
-            int rowsAffected = stmt.executeUpdate();
-            System.out.println(rowsAffected + " row(s) inserted.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executeInsert(
+                "INSERT INTO colegperry.course VALUES (?, ?, ?, TO_DATE(?, 'YYYY-MM-DD'), TO_DATE(?, 'YYYY-MM-DD'))",
+                params);
     }
 
     private static void insertTrainerRecord() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Trainer ID, Trainer Name, Phone Number (comma-separated): ");
+        String input = scanner.nextLine();
+        String[] params = input.split(",");
 
-        System.out.println("Enter Trainer ID: ");
-        int trainerId = scanner.nextInt();
-        scanner.nextLine(); // consume newline
-
-        System.out.println("Enter Trainer Name: ");
-        String trainerName = scanner.nextLine();
-
-        System.out.println("Enter Phone Number: ");
-        int phoneNumber = scanner.nextInt();
-
-        try (Connection conn = DBConnection.getConnection();
-                PreparedStatement stmt = conn.prepareStatement("INSERT INTO colegperry.trainer VALUES (?, ?, ?)")) {
-
-            stmt.setInt(1, trainerId);
-            stmt.setString(2, trainerName);
-            stmt.setInt(3, phoneNumber);
-
-            int rowsAffected = stmt.executeUpdate();
-            System.out.println(rowsAffected + " row(s) inserted.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executeInsert("INSERT INTO colegperry.trainer VALUES (?, ?, ?)", params);
     }
 
     private static void insertTrainClassRecord() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Trainer ID, Class ID (comma-separated): ");
+        String input = scanner.nextLine();
+        String[] params = input.split(",");
 
-        System.out.println("Enter Trainer ID: ");
-        int trainerId = scanner.nextInt();
-
-        System.out.println("Enter Class ID: ");
-        int classId = scanner.nextInt();
-
-        try (Connection conn = DBConnection.getConnection();
-                PreparedStatement stmt = conn.prepareStatement("INSERT INTO colegperry.trainClass VALUES (?, ?)")) {
-
-            stmt.setInt(1, trainerId);
-            stmt.setInt(2, classId);
-
-            int rowsAffected = stmt.executeUpdate();
-            System.out.println(rowsAffected + " row(s) inserted.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executeInsert("INSERT INTO colegperry.trainClass VALUES (?, ?)", params);
     }
 
     private static void insertItemRecord() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                "Enter Item ID, Member ID, Check-in Time, Check-out Time, Quantity, Lost Status (0 or 1) (comma-separated): ");
+        String input = scanner.nextLine();
+        String[] params = input.split(",");
 
-        System.out.println("Enter Item ID: ");
-        int itemId = scanner.nextInt();
-
-        System.out.println("Enter Member ID: ");
-        int memberId = scanner.nextInt();
-
-        System.out.println("Enter Check-in Time (as an integer): ");
-        int checkin = scanner.nextInt();
-
-        System.out.println("Enter Check-out Time (as an integer): ");
-        int checkout = scanner.nextInt();
-
-        System.out.println("Enter Quantity: ");
-        int qty = scanner.nextInt();
-
-        System.out.println("Enter Lost Status (0 for not lost, 1 for lost): ");
-        int lost = scanner.nextInt();
-
-        try (Connection conn = DBConnection.getConnection();
-                PreparedStatement stmt = conn
-                        .prepareStatement("INSERT INTO colegperry.item VALUES (?, ?, ?, ?, ?, ?)")) {
-
-            stmt.setInt(1, itemId);
-            stmt.setInt(2, memberId);
-            stmt.setInt(3, checkin);
-            stmt.setInt(4, checkout);
-            stmt.setInt(5, qty);
-            stmt.setInt(6, lost);
-
-            int rowsAffected = stmt.executeUpdate();
-            System.out.println(rowsAffected + " row(s) inserted.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executeInsert("INSERT INTO colegperry.item VALUES (?, ?, ?, ?, ?, ?)", params);
     }
 
     private static void insertTransactionRecord() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                "Enter Transaction ID, Amount, Transaction Date (yyyy-mm-dd), Transaction Type (comma-separated): ");
+        String input = scanner.nextLine();
+        String[] params = input.split(",");
 
-        System.out.println("Enter Transaction ID: ");
-        int transId = scanner.nextInt();
-
-        System.out.println("Enter Amount: ");
-        int amount = scanner.nextInt();
-
-        System.out.println("Enter Transaction Date (format yyyy-mm-dd): ");
-        String transDate = scanner.next();
-
-        System.out.println("Enter Transaction Type: ");
-        scanner.nextLine(); // consume newline
-        String transType = scanner.nextLine();
-
-        try (Connection conn = DBConnection.getConnection();
-                PreparedStatement stmt = conn.prepareStatement(
-                        "INSERT INTO colegperry.transaction VALUES (?, ?, TO_DATE(?, 'YYYY-MM-DD'), ?)")) {
-
-            stmt.setInt(1, transId);
-            stmt.setInt(2, amount);
-            stmt.setString(3, transDate);
-            stmt.setString(4, transType);
-
-            int rowsAffected = stmt.executeUpdate();
-            System.out.println(rowsAffected + " row(s) inserted.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executeInsert("INSERT INTO colegperry.transaction VALUES (?, ?, TO_DATE(?, 'YYYY-MM-DD'), ?)", params);
     }
 
     private static void insertCoursePackageRecord() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Package Number, Package Name, First Class ID, Second Class ID (comma-separated): ");
+        String input = scanner.nextLine();
+        String[] params = input.split(",");
 
-        System.out.println("Enter Package Number: ");
-        int packageNum = scanner.nextInt();
+        executeInsert("INSERT INTO colegperry.coursePackage VALUES (?, ?, ?, ?)", params);
+    }
 
-        System.out.println("Enter Package Name: ");
-        scanner.nextLine(); // consume newline
-        String packageName = scanner.nextLine();
-
-        System.out.println("Enter First Class ID: ");
-        int firstClassId = scanner.nextInt();
-
-        System.out.println("Enter Second Class ID: ");
-        int secondClassId = scanner.nextInt();
-
+    private static void executeInsert(String sql, String[] params) {
         try (Connection conn = DBConnection.getConnection();
-                PreparedStatement stmt = conn
-                        .prepareStatement("INSERT INTO colegperry.coursePackage VALUES (?, ?, ?, ?)")) {
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-            stmt.setInt(1, packageNum);
-            stmt.setString(2, packageName);
-            stmt.setInt(3, firstClassId);
-            stmt.setInt(4, secondClassId);
+            for (int i = 0; i < params.length; i++) {
+                stmt.setString(i + 1, params[i]);
+            }
 
             int rowsAffected = stmt.executeUpdate();
             System.out.println(rowsAffected + " row(s) inserted.");
---
+++ docs/sql/initial_data_inserts.sql
@@ -0,0 +1,88 @@
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (1, 'First1', 'Last1', '123456781', 1, 100.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (2, 'First2', 'Last2', '123456782', 1, 200.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (3, 'First3', 'Last3', '123456783', 1, 300.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (4, 'First4', 'Last4', '123456784', 1, 400.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (5, 'First5', 'Last5', '123456785', 1, 500.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (6, 'First6', 'Last6', '123456786', 1, 600.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (7, 'First7', 'Last7', '123456787', 1, 700.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (8, 'First8', 'Last8', '123456788', 1, 800.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (9, 'First9', 'Last9', '123456789', 1, 900.0);
+INSERT INTO colegperry.member (memberID, fname, lname, phonenum, memlevelID, totalspent) VALUES (10, 'First10', 'Last10', '1234567810', 1, 1000.0);
+
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (1, 'Gold', 15, 500);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (2, 'Silver', 10, 300);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (3, 'Level3', 5, 100);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (4, 'Level4', 5, 100);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (5, 'Level5', 5, 100);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (6, 'Level6', 5, 100);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (7, 'Level7', 5, 100);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (8, 'Level8', 5, 100);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (9, 'Level9', 5, 100);
+INSERT INTO colegperry.memlevel (levelID, levelname, discount, minspent) VALUES (10, 'Level10', 5, 100);
+
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (1, 'Course1', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (2, 'Course2', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (3, 'Course3', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (4, 'Course4', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (5, 'Course5', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (6, 'Course6', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (7, 'Course7', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (8, 'Course8', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (9, 'Course9', 10, '2023-01-01', '2023-12-31');
+INSERT INTO colegperry.course (courseID, courseName, time, sdate, edate) VALUES (10, 'Course10', 10, '2023-01-01', '2023-12-31');
+
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (1, 'Trainer1', '987654321');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (2, 'Trainer2', '987654322');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (3, 'Trainer3', '987654323');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (4, 'Trainer4', '987654324');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (5, 'Trainer5', '987654325');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (6, 'Trainer6', '987654326');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (7, 'Trainer7', '987654327');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (8, 'Trainer8', '987654328');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (9, 'Trainer9', '987654329');
+INSERT INTO colegperry.trainer (trainerID, trainername, phonenum) VALUES (10, 'Trainer10', '9876543210');
+
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (1, 1);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (2, 2);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (3, 3);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (4, 4);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (5, 5);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (6, 6);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (7, 7);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (8, 8);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (9, 9);
+INSERT INTO colegperry.trainClass (trainerID, classID) VALUES (10, 10);
+
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (1, 1, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (2, 2, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (3, 3, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (4, 4, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (5, 5, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (6, 6, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (7, 7, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (8, 8, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (9, 9, 8, 18, 1, 0);
+INSERT INTO colegperry.item (itemID, memberID, checkin, checkout, qty, lost) VALUES (10, 10, 8, 18, 1, 0);
+
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (1, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (2, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (3, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (4, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (5, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (6, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (7, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (8, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (9, 100, '2023-06-01', 'Purchase');
+INSERT INTO colegperry.transaction (transID, amount, transdate, transtype) VALUES (10, 100, '2023-06-01', 'Purchase');
+
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (1, 'Package1', 1, 2);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (2, 'Package2', 2, 3);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (3, 'Package3', 3, 4);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (4, 'Package4', 4, 5);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (5, 'Package5', 5, 6);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (6, 'Package6', 6, 7);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (7, 'Package7', 7, 8);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (8, 'Package8', 8, 9);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (9, 'Package9', 9, 10);
+INSERT INTO colegperry.coursePackage (packagenum, packagename, firstclassID, secondclassID) VALUES (10, 'Package10', 10, 11);
+
