55f7919cd419b3bf4b0d9a613794cf8474203856
Jollyolfun <90577117+Jollyolfun@users.noreply.github.com>
2023-11-29 18:00:47 -0700

    Logged into Cole's account to allow querying

--- DBConnection.java
+++ DBConnection.java
@@ -3,8 +3,8 @@ import java.sql.*;
 
 public class DBConnection {
     private static final String URL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:orcl";
-    private static final String USER = "hwp";
-    private static final String PASSWORD = "a2301";
+    private static final String USER = "colegperry";
+    private static final String PASSWORD = "a7601";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
